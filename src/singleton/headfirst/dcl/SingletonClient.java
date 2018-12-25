51e0ff27629d64cecd750d11de13286ddf716bcf
Sumit Saha <sumit.saha@appdynamics.com>
2018-12-25 18:56:08 +0530

    adding singelton files

---
+++ src/singleton/headfirst/dcl/SingletonClient.java
@@ -0,0 +1,9 @@
+package singleton.headfirst.dcl;
+
+public class SingletonClient {
+	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton);
+		System.out.println(singleton);
+	}
+}
